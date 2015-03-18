532044559365ba798df5559877263cb4ebd77c94
Benno van den Berg <bennovandenberg@gmail.com>
2015-03-18 12:23:16 +0100

    ubuntu

--- install-phpenv.sh
+++ install-phpenv.sh
@@ -3,3 +3,6 @@ git clone https://github.com/CHH/phpenv.git
 sudo ./phpenv/bin/phpenv-install.sh
 echo 'export PATH="/home/ubuntu/.phpenv/shims:/home/ubuntu/.phpenv/bin:$PATH"' >> $HOME/.bash_profile
 echo 'eval "$(phpenv init -)"' >> $HOME/.bash_profile
+
+chown -R ubuntu $HOME
+chown -R ubuntu /usr/local/share/php-build
